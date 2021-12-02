61708fa89f36bfaab667947e02f74fcf3b35a06e
zsharpm <zachmsharp@gmail.com>
2021-12-01 22:09:36 -0600

    static file

--- prob01.py
+++ prob01.py
@@ -12,20 +12,18 @@ def label(x):
         return "(N/A - no previous measurement)"
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        file_location = sys.argv[1].strip()
-        df = pd.read_table("data/prob1", header=None, names=["val"])
-        df['val_lag'] = df['val'].shift(1)
-        df['roll_sum'] = df['val'].rolling(window=3).sum()
-        df['roll_sum_lag'] = df['roll_sum'].shift(1)
-        df['label'] = df[["val", "val_lag"]].apply(label, axis=1)
-        df['label2'] = df[["roll_sum", "roll_sum_lag"]].apply(label, axis=1)
+    df = pd.read_table("data/prob1", header=None, names=["val"])
+    df['val_lag'] = df['val'].shift(1)
+    df['roll_sum'] = df['val'].rolling(window=3).sum()
+    df['roll_sum_lag'] = df['roll_sum'].shift(1)
+    df['label'] = df[["val", "val_lag"]].apply(label, axis=1)
+    df['label2'] = df[["roll_sum", "roll_sum_lag"]].apply(label, axis=1)
 
-        mask = df['label'] == "(increased)"
-        print(df[mask]['label'].count())
-        mask2 = df['label2'] == "(increased)"
-        print(df[mask2]['label2'].count())
-    else:
+    mask = df['label'] == "(increased)"
+    print(df[mask]['label'].count())
+    mask2 = df['label2'] == "(increased)"
+    print(df[mask2]['label2'].count())
+else:
         print('This test requires an input file.')
 
 
